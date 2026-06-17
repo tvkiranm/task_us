2c0e7fefb9c8b6426a29755fed03e4455a546b2d
tvkiranm <kiran@techvoot.com>
2026-06-17 17:35:53 +0530

    Remove old frontend and backend

--- task-api/src/modules/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login } = require("./auth.controller");
-
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-module.exports = router;
--- task-api/src/modules/tasks/task.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAllTasks, createTask } = require("./task.controller");
-
-const router = express.Router();
-
-router.route("/").get(getAllTasks).post(createTask);
-
-module.exports = router;
